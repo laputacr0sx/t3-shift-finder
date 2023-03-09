be315d41587c14f5518994348ac1668bee168e96
Veilics <>
2023-03-09 18:08:47 +0800

    nextImage - nav bar implemented with google avatar

--- next.config.mjs
+++ next.config.mjs
@@ -20,5 +20,13 @@ const config = {
     locales: ["en"],
     defaultLocale: "en",
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+    ],
+  },
 };
 export default config;
